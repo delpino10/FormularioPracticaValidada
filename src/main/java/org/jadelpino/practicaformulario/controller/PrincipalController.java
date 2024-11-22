8b13e3648928f50e16bcc6600987e978960dbe7d
Jose Antonio del Pino <jadelpino10@gmail.com>
2024-11-22 19:44:48 +0100

    El Formulario pinta y repinta

--- src/main/java/org/jadelpino/practicaformulario/controller/PrincipalController.java
+++ src/main/java/org/jadelpino/practicaformulario/controller/PrincipalController.java
@@ -21,7 +21,6 @@ public class PrincipalController {
     int interaccion = 1;
 
 
-
     @ModelAttribute("lista_paises")
     private Map<String, Pais> devuelveListaPaices() {
         return Colecciones.getListaPaises();
@@ -46,18 +45,47 @@ public class PrincipalController {
     // Envía la plantilla del formulario a rellenar por el usuario
     @GetMapping("devuelve-formulario")
     public String devuelveFormulario(
-            @ModelAttribute ("datosFormulario") DatosFormulario datosFormulario,
-            Model modelo) {
-        // Titulo
-        String titulo = " Original";
+            Model modelo,
+            @ModelAttribute ("datosFormulario") DatosFormulario datosFormulario) {
 
         // Titulo
-        modelo.addAttribute("titulo", titulo);
+        modelo.addAttribute("titulo", "Original");
+        // Licencia por defecto desactivada
+        modelo.addAttribute("licencia", false);
         // Interacciones
-       modelo.addAttribute("interaccion", interaccion);
+       // modelo.addAttribute("interaccion", interaccion);
 
         return "formulario";
     }
+
+
+    // Recibe los parámetros del formulario y los pasa de nuevo a la vista
+    @PostMapping("recibeParametrosYRepinta")
+    public String recibeParametros(
+
+            // @ModelAttribute sirve para pasar datos al modelo de la vista.
+            // Si se quita @ModelAttribute, habría que pasarlo con model.addAttribute
+            // modelo.addAttribute("datos-formulario", datosFormulario);
+            @Valid @ModelAttribute DatosFormulario datosFormulario,
+            BindingResult bindingResult,
+            // accedemos al objeto que muestran las coordenadas en x
+            @RequestParam(name = "imagen.x", required = false) Integer x,
+            // accedemos al objeto que muestran las coordenadas en y
+            @RequestParam(name = "imagen.y", required = false) Integer y,
+            Model modelo) {
+
+        //Título
+        modelo.addAttribute("titulo", " Repintado");
+
+        // Mostrar el mensaje de las coordenadas en la vista
+        String coordenadas = coordenadasImage(x,y);
+        modelo.addAttribute("coordenadas", coordenadas);
+
+        System.err.println(datosFormulario.toString());
+
+        return "formulario";
+    }
+
 
     // Desactiva la peticion del favicon.ico
     @Controller
@@ -69,46 +97,12 @@ public class PrincipalController {
         }
     }
 
-    // Recibe los parámetros del formulario y los pasa de nuevo a la vista
-    @PostMapping("recibeParametrosYRepinta")
-    public String recibeParametros(
-            @Valid DatosFormulario datosFormulario,
-            BindingResult bindingResult,
-            // accedemos al objeto que muestran las coordenadas en x
-            @RequestParam(name = "imagen.x", required = false) Integer x,
-            // accedemos al objeto que muestran las coordenadas en y
-            @RequestParam(name = "imagen.y", required = false) Integer y,
-            Model modelo
-            ) {
-
-        if(bindingResult.hasErrors()) {// Si hay errores
-            modelo.addAttribute("mensajeNOK", "El formulario contiene errores");
-            // Suma uno cada vez que pulsamos enviar
-            interaccion++;
-            modelo.addAttribute("interaccion", interaccion);
-            // Pinta Repinatdo el título
-            modelo.addAttribute("titulo", " Repintado");
-
-            return "formulario";
-        }
-        // Si no hay errores
-        modelo.addAttribute("mensajeOk", "El formulario no contiene errores");
-
-        System.err.println(datosFormulario.toString());
-
-
-        // Pinta Repinatdo el título
-        modelo.addAttribute("titulo", " Repintado");
-
-        // Suma uno cada vez que pulsamos enviar
-        interaccion++;
-        modelo.addAttribute("interaccion", interaccion);
-
         // Mostrar el mensaje de las coordenadas en la vista
-        String coordenadas = coordenadasImage(x,y);
-        modelo.addAttribute("coordenadas", coordenadas);
-
-        return "formulario";
+    public static String coordenadasImage (Integer x , Integer y){
+        String coordenadas = (x != null && y != null)
+                ? "imagen.x: " + x + " e imagen.y: " + x
+                : "";
+        return coordenadas;
     }
 
     @PostMapping("rec-prueba")
@@ -119,12 +113,5 @@ public class PrincipalController {
         return "formulario";
     }
 
-        // Mostrar el mensaje de las coordenadas en la vista
-    public static String coordenadasImage (Integer x , Integer y){
-        String coordenadas = (x != null && y != null)
-                ? "imagen.x: " + x + " e imagen.y: " + x
-                : "";
-        return coordenadas;
-    }
 
 }
--- src/main/java/org/jadelpino/practicaformulario/model/DatosFormulario.java
+++ src/main/java/org/jadelpino/practicaformulario/model/DatosFormulario.java
@@ -1,6 +1,5 @@
 package org.jadelpino.practicaformulario.model;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -10,43 +9,39 @@ import java.util.List;
 @NoArgsConstructor @AllArgsConstructor
 @Getter @Setter @ToString
 public class DatosFormulario {
-    @NotBlank(message = "Por favor, rellene el campo usuario")
-    private String usuario = "Lola"; // String Lola como valor por defecto a la vista
+
+    // String Lola como valor por defecto a la vista
+    private String usuario = "Lola";
     private String clave;
     private String confirmarClave;
     private String generoSeleccionado;
-    //Portugal como pais por defecto a la vista
     private String paisSeleccionado = "pt";
     private LocalDate fechaNacimiento;
     private Integer edad;
     private Float peso;
-    // Prefijo Francia como Prefijo por defecto a la vista
     private String prefijoTelefonico = "fr";
     private String telefono;
     private String email;
     private String url;
     private String archivos;
-    // Pasamos los valores de las musicas por defecto a la vista
     private List<String> musicasSeleccionadas = musicasDefecto();
-    // Pasamos los valores de las aficiones por defecto a la vista
     private List<String> aficionesSeleccionadas = aficionesDefecto();
     private String comentarios;
     private Boolean licencia;
 
-    // Guardamos en un Arraylist los valores por defecto de aficiones
     private List<String> aficionesDefecto(){
-        List<String> aficionesDefecto = new ArrayList<String>();
-        aficionesDefecto.add("D");
-        aficionesDefecto.add("P");
-        aficionesDefecto.add("V");
-        return aficionesDefecto;
-
-    }// Guardamos en un Arraylist los valores por defecto de musica
-    private List<String> musicasDefecto(){
-        List<String> musicasDefecto = new ArrayList<String>();
-        musicasDefecto.add("F");
-        musicasDefecto.add("P");
-        return musicasDefecto;
+        aficionesSeleccionadas = new ArrayList<>();
+        aficionesSeleccionadas.add("D");
+        aficionesSeleccionadas.add("P");
+        aficionesSeleccionadas.add("V");
+        return aficionesSeleccionadas;
     }
 
+    private List<String> musicasDefecto(){
+        musicasSeleccionadas = new ArrayList<>();
+        musicasSeleccionadas.add("F");
+        musicasSeleccionadas.add("P");
+        return musicasSeleccionadas;
+    }
 }
+
